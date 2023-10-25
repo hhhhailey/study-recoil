8c6e5c4fae99593f71fde97d258e12e62651981d
hailey <gmlthd0908@gmail.com>
2023-10-25 18:50:23 +0900

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.e495f556.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstudy_recoil=self.webpackChunkstudy_recoil||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.e495f556.chunk.js.map
